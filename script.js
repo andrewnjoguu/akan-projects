921c692e977eef1f9f9c9325256ef0fc8b73c762
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-09-04 23:56:03 +0300

    added control flow

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ const giveAkan= function (e) {
     const fd = new FormData(frm)
     const gender = fd.get("gender");
     const dob = fd.get("dob")
-
-
+    
     const d = new Date(dob).getDay();
-    alert("Your Akan name is" + fNames[d])
-}
+    //alert("Your Akan name is" + fNames[d])
 
+    const successAlert = document.querySelector("success-alert")
+
+    successAlert.classList.remove("d-none");
+    successAlert.classList.add("d-block")
+    
+
+    if(gender = "female") {
+        alert("Your Akan name is" +fNames[d])
+    }else{
+        alert("Your Akan name is" +mNames[d])
+    }
+    //alert("Your Akan name is" + fNames[d])
+  
+    
+}
+    
 
 document.querySelector("#frmAkans").addEventListener('sumbit',giveAkan);
