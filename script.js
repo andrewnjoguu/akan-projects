c163ab98e03f6a2d6584ec3f5253d4fb3b6e01b5
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-09-04 17:56:06 +0300

    added Akan names to JS

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const akannames - ()
+const getakannames= function (e) {
+    e.preventDefault();
+    const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+    const mNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"}
+}
+console.log ("gender")
