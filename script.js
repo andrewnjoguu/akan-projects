074ec64aabd3b083f400e303f7f4db7269d79cd3
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-09-04 22:53:37 +0300

    added form on js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const getakannames= function (e) {
     e.preventDefault();
-    const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-    const mNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"}
+    const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua" , "Yaa", "Afua", "Ama"]
+    const mNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    const frm = document.querySelector("#frmAkans")
+    const fd = new FormData(frm)
+    const gender = fd.get("gender");
+    const date of birth = fd.get("Date of birth")
+
+    console.log(fNames[0])
 }
-console.log ("gender")
+
+
+document.querySelector("#frmAkans").addEventListener('sumbit' getakannames);
