8df23d290a32961191a7a920df92cbb0076d1506
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-09-06 15:15:37 +0300

    added background

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const giveAkan =  (e) => {
     e.preventDefault();
+    
     const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     const mNames = [ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame",];
      
