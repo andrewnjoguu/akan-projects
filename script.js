65b5acbe8d5cc03fc4e290646279c651b5a6cf29
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-09-06 11:23:27 +0300

    used queryselector

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-const giveAkan = function (e) {
-  e.preventDefault();
-  const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  const mNames = [
-    "Kwasi",
-    "Kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "Kwame",
-  ];
-
-  const dateOfBirth = document.getElementById("dob").value;
-  if (!dateOfBirth) {
-    return alert("Enter Birthday");
-  }
-
-  const d = new Date(dateOfBirth).getDay();
-  const fGender = document.getElementById("female").checked;
-  const mGender = document.getElementById("male").checked;
-
-  if (fGender ==="female") {
-    alert("Your Akan name is" + fNames[d]);
-  } else if (mGender) {
-    alert("Your Akan name is" + mNames[d]);
-  } else {
-    return alert("Please select gender.");
-  }
-};
-
-document.querySelector("#frmAkans").addEventListener("sumbit", giveAkan);
+const giveAkan =  (e) => {
+    e.preventDefault();
+    const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    const mNames = [ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame",];
+     
+    const dateOfBirth = document.getElementById("dob").value;
+    if (!dateOfBirth) {
+      return alert("Enter Birthday");
+    }
+  
+    const d = new Date(dateOfBirth).getDay();
+    const fGender = document.getElementById("female").checked;
+    const mGender = document.getElementById("male").checked;
+  
+    if (fGender ==="female") {
+      alert("Your Akan name is" + fNames[d]);
+    } else if (mGender) {
+        alert("Your Akan name is" + mNames[d]);
+      } else {
+        return alert("Please select gender.");
+      }
+    };
+document.querySelector("#frmAkans").addEventListener("sumbit", giveAkan)
