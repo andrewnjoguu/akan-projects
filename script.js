7fbf0842b47a2b43be9fe0784a3a23942c646d4a
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-09-09 20:37:20 +0300

    corrected spelling error

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
 const giveAkan =  (e) => {
     e.preventDefault();
-    
+  
+    console.log ("giveAkan")
     const fNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     const mNames = [ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame",];
      
@@ -8,17 +10,18 @@ const giveAkan =  (e) => {
     if (!dateOfBirth) {
       return alert("Enter Birthday");
     }
-  
+    
     const d = new Date(dateOfBirth).getDay();
     const fGender = document.getElementById("female").checked;
     const mGender = document.getElementById("male").checked;
   
-    if (fGender ==="female") {
-      alert("Your Akan name is" + fNames[d]);
+    if (fGender) {
+      alert("Your Akan name is " + fNames[d]);
     } else if (mGender) {
-        alert("Your Akan name is" + mNames[d]);
+        alert("Your Akan name is " + mNames[d]);
       } else {
         return alert("Please select gender.");
       }
     };
-document.querySelector("#frmAkans").addEventListener("sumbit", giveAkan)
+    
+document.querySelector("#frmAkans").addEventListener("submit", giveAkan)
